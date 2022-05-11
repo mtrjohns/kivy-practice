1ad94fc91ca342865ab717dc950b6287385dae26
mtrjohns <mtrj1776@hotmail.co.uk>
2022-05-11 23:03:19 +0100

    Stack Layout

---
+++ pagelayout/pagelayout.py
@@ -0,0 +1,13 @@
+import kivy
+kivy.require('1.9.0')
+
+from kivy.app import App
+from kivy.uix.pagelayout import PageLayout
+
+class PageLayoutApp(App):
+    
+    def build(self):
+        return PageLayout()
+    
+plApp = PageLayoutApp()
+plApp.run()
