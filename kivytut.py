82c55e63455522769869d3d3903ee0279a5f049f
mtrjohns <mtrj1776@hotmail.co.uk>
2022-05-11 21:02:25 +0100

    Initial Hello Kivy example with .kv file

---
+++ kivytut.py
@@ -0,0 +1,15 @@
+import kivy
+kivy.require('1.9.0')
+
+from kivy.app import App
+from kivy.uix.button import Label
+
+# hellokivy.kv will need to be made to assign it to the HelloKivyApp
+class HelloKivyApp(App):
+
+    def build(self):
+        return Label()
+    
+helloKivy = HelloKivyApp()
+    
+helloKivy.run()    
