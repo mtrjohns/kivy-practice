3ef2de70bc3011633c2a0829040fade694959ffe
mtrjohns <mtrj1776@hotmail.co.uk>
2022-05-12 09:08:18 +0100

    Caculator

---
+++ calculator/calculator.py
@@ -0,0 +1,24 @@
+from grpc import GenericRpcHandler
+import kivy
+from numpy import gradient
+kivy.require("1.9.0")
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+
+class CalcGridLayout(GridLayout):
+    
+    def calculate(self, calculation):
+        if calculation:
+            try:
+                self.display.text = str(eval(calculation))
+            except Exception:
+                self.display.text = "Error"
+
+class CalculatorApp(App):
+    
+    def build(self):
+        return CalcGridLayout()
+    
+calcApp = CalculatorApp()
+calcApp.run()
